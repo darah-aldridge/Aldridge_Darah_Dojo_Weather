39cd671465fdaad73f949ce06ad9e6678b76b5c3
Darah Aldridge <darah.aldridge@gmail.com>
2022-02-03 20:36:25 -0600

    Completed Assignment - Dojo Weather

---
+++ script.js
@@ -0,0 +1,8 @@
+var tempScale = document.querySelector("#pick-temp");
+
+var cookieBanner = document.getElementById("cookiebanner");
+
+
+function acceptCookies(){
+    cookieBanner.parentNode.removeChild(cookieBanner);
+}
